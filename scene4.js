811b7a2b1a56bc03ebe766cfb411e96a71bfb03e
schottlw <schottlw@mail.uc.edu>
2021-08-09 11:34:30 -0400

    Changed game over text to red

--- scene4.js
+++ scene4.js
@@ -10,19 +10,22 @@ class Scene4 extends Phaser.Scene{
 
     create(){
         this.add.text(this.scale.width * 0.5, this.scale.height * 0.5, 'Game Over', {
-            fonstSize: 24
+            fonstSize: 24,
+            color: '#FF0000'
 
         })
         .setOrigin(0.5); // sets text to middle of screen
 
         this.add.text(this.scale.width * 0.5, this.scale.height * 0.53, 'Stars: ' + this.score, {
-            fonstSize: 24
+            fonstSize: 24,
+            color: '#FF0000'
 
         })
         .setOrigin(0.5);
 
         this.add.text(this.scale.width *0.5, this.scale.height * 0.56, 'Press SPACE to restart', {
-            fontSize: 18
+            fontSize: 18,
+            color: '#FF0000'
         })
         .setOrigin(0.5);
 
